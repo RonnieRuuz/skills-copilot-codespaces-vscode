fbf1dc1d2ff76637e8f5cc640fe6ddd3cae69dd8
Ronnie Garcia Ruiz <ronnie.ruuz@gmail.com>
2023-11-28 00:32:01 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillMember() {
+    return {
+        name: 'skillMember',
+        restrict: 'E',
+        templateUrl: 'templates/skill-member.html',
+        controller: 'SkillMemberCtrl',
+        controllerAs: 'skillMemberCtrl'
+    };
+}
